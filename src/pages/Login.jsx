fd9a67c27fb5baac0c12da27324391762fc89a34
ADISHAR2005 <sharadityaa@gmail.com>
2026-05-31 11:23:48 +0530

    Login page done

---
+++ src/pages/Login.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import InputField from "../components/InputField";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-md bg-white p-8 rounded-xl shadow-lg">
+        <h1 className="text-3xl font-bold text-center mb-2">
+          Welcome Back
+        </h1>
+
+        <p className="text-center text-gray-500 mb-6">
+          Sign in to your account
+        </p>
+
+        <form className="space-y-4">
+          <InputField
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <div className="relative">
+            <InputField
+              type={showPassword ? "text" : "password"}
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500"
+            >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </button>
+          </div>
+
+          {/* <div className="flex justify-end">
+            <a
+              href="#"
+              className="text-sm text-blue-600 hover:underline"
+            >
+              Forgot Password?
+            </a>
+          </div> */}
+
+          <button
+            type="submit"
+            className="w-full bg-black text-white py-3 rounded-lg hover:opacity-90 transition"
+          >
+            Login
+          </button>
+        </form>
+
+        <p className="text-center text-gray-500 mt-6">
+          Dont have an account?{" "}
+          <span className="text-blue-600 cursor-pointer">
+            Sign Up
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
